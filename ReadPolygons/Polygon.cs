e85cbe4efcb6e2a65e6e5a96e2864a4d4fd28a8f
Alexandre Laptev <alex_laptev@hotmail.com>
2020-03-31 10:45:26 -0700

    Added project to repository.

---
+++ ReadPolygons/Polygon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ReadPolygons
+{
+    internal class Polygon
+    {
+        public Polygon(double upperLeftLongitude, double upperLeftLatitude, double lowerRightLongitude, double lowerRightLatitude)
+        {
+            //Verify points are upper left and lower right corners
+            if (upperLeftLongitude > 0 && lowerRightLongitude < 0)
+            {
+                //Spans Crosses 180 and -180
+                if (upperLeftLatitude < 0 && lowerRightLatitude > 0)
+                {
+                    //Spans Crosses 90 and -90; is valid
+                }
+                else
+                {
+                    if (upperLeftLatitude <= lowerRightLatitude)
+                    {
+                        //Corners are not correct
+                        throw new ArgumentException("Coordinates do not define the upper left and lower right corners of the box.");
+                    }
+                }
+            }
+            else
+            {
+                if ((upperLeftLongitude >= lowerRightLongitude) ||
+                    (upperLeftLatitude <= lowerRightLatitude))
+                {
+                    //Corners are not correct
+                    throw new ArgumentException("Coordinates do not define the upper left and lower right corners of the box.");
+                }
+            }
+
+            UpperLeftLongitude = upperLeftLongitude;
+            UpperLeftLatitude = upperLeftLatitude;
+            LowerRightLongitude = lowerRightLongitude;
+            LowerRightLatitude = lowerRightLatitude;
+        }
+
+        public double UpperLeftLongitude { get; private set; }
+        public double UpperLeftLatitude { get; private set; }
+        public double LowerRightLongitude { get; private set; }
+        public double LowerRightLatitude { get; private set; }
+    }
+}
---
+++ ReadPolygons/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace ReadPolygons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Get the current directory and make it a DirectoryInfo object.
+                // Do not use Environment.CurrentDirectory, vistual studio 
+                // and visual studio code will return different result:
+                // Visual studio will return @"projectDir\bin\Release\netcoreapp2.0\", yet 
+                // vs code will return @"projectDir\"
+                var currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+
+                // On windows, the current directory is the compiled binary sits,
+                // so string like @"bin\Release\netcoreapp2.0\" will follow the project directory. 
+                // Hense, the project directory is the great grand-father of the current directory.
+                string projectDirectory = currentDirectory.Parent.Parent.Parent.FullName;
+
+                string polygonPath = Path.Combine(projectDirectory, "Polygons.xml");
+
+                var polygons = new List<Polygon>();
+
+                var xelement = XElement.Load(polygonPath);
+                var xelements = xelement.Elements();
+
+                foreach (var e in xelements)
+                {
+                    var polygon = new Polygon
+                        (
+                            double.Parse(e.Element("UpperLeftLongitude").Value),
+                            double.Parse(e.Element("UpperLeftLatitude").Value),
+                            double.Parse(e.Element("LowerRightLongitude").Value),
+                            double.Parse(e.Element("LowerRightLatitude").Value)
+                        );
+
+                    polygons.Add(polygon);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{polygons.Count} loaded from '{polygonPath}' file.");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
